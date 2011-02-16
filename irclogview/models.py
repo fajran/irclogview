5632daecf9c5f271eeba0f9948d88f44d6a070d0
Fajran Iman Rusadi <frusadi@science.uva.nl>
2011-02-16 11:56:48 +0100

    Add function to get content in list of dicts format

--- irclogview/models.py
+++ irclogview/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from picklefield.fields import PickledObjectField
 
+from . import utils
+
 class Channel(models.Model):
     name = models.SlugField(max_length=50, unique=True)
     updated = models.DateTimeField(auto_now=True)
@@ -18,4 +20,11 @@ class Log(models.Model):
 
     class Meta:
         unique_together = ('channel', 'date')
+
+    def content_dict(self):
+        colors = utils.RainbowColor()
+        for data in self.content:
+            item = dict(zip(['time', 'type', 'name', 'text'], data))
+            item['name_color'] = colors.get_color(item['name'])
+            yield item
 
