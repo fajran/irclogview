ae0533052d1d0c13a52c0978dd5f46e38d15fa69
Fajran Iman Rusadi <frusadi@science.uva.nl>
2011-02-16 11:59:25 +0100

    Add license: AGPL 3.0

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name = 'irclogview',
     version = '0.1',
     url = 'https://github.com/fajran/irclogview',
+    license = 'AGPL 3.0',
     description = 'IRC Log viewer for Django',
     author = 'Fajran Iman Rusadi',
     packages = packages,
