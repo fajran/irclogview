60df8c30465f0688aba1a872c71421b14eb5bc6b
Fajran Iman Rusadi <fajran@gmail.com>
2011-08-28 20:18:44 +0200

    Use render_to_response

--- irclogview/views.py
+++ irclogview/views.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-
-from annoying.decorators import render_to
+from django.shortcuts import get_object_or_404, render_to_response
 
 from .models import Channel, Log
 
@@ -14,12 +12,12 @@ def channel_index(request, name):
     channel = get_object_or_404(Channel, name=name)
     return HttpResponse('channel: %s' % name)
 
-@render_to('irclogview/show_log.html')
 def show_log(request, name, year, month, day):
     channel = get_object_or_404(Channel, name=name)
     date = datetime(int(year), int(month), int(day)).date()
 
     log = get_object_or_404(Log, channel=channel, date=date)
 
-    return {'log': log}
+    context = {'log': log}
+    return render_to_response('irclogview/show_log.html', context)
 
