641a12d42bea5a78f8b1188ea758075fec6ef445
Fajran Iman Rusadi <fajran@gmail.com>
2011-09-12 23:58:12 +0200

    Set /media/ as static folder

--- irclogview/urls.py
+++ irclogview/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
+from django.conf import settings
 
-urlpatterns = patterns('irclogview.views',
+urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
+)
+
+urlpatterns += patterns('irclogview.views',
     url(r'^(?P<name>[^/]+)/(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})/$', 'show_log', name='irclogview_show'),
     url(r'^(?P<name>[^/]+)/$', 'channel_index', name='irclogview_channel'),
     url(r'^$', 'index', name='irclogview_index'),
