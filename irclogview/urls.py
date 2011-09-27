a23146b1685bdcd68bc56ac45243a49323d3065d
Fajran Iman Rusadi <fajran@gmail.com>
2011-09-27 21:17:05 +0200

    Enable admin page

--- irclogview/urls.py
+++ irclogview/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, url, include
 from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += patterns('irclogview.views',
